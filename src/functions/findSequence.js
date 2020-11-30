7234c22b0a4716249c7476f1d168eaec4b8ea701
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-11-30 19:09:16 +0100

    Implement custom search routine for array of arrays

--- src/functions/findSequence.js
+++ src/functions/findSequence.js
@@ -2,105 +2,89 @@ export default function findSequence (grid, rowRootIndex, columnRootIndex) {
 
     const colorIndex = grid[rowRootIndex][columnRootIndex];
     let identicalNeighbours = [[rowRootIndex, columnRootIndex]];
+    let isLookingIdenticalNeighbours = true;
 
-    identicalNeighbours.forEach(coordinates => {
+    function isNeighbourAlreadyAdded(inputArray) {
+        let didFindElement = false;
+        identicalNeighbours.forEach(element => {
+            if (element[0] === inputArray[0] && element[1] === inputArray[1]) {
+                didFindElement = true;
+            }
+        });
 
-        // const isTopNeighbourIdentical = grid [coordinates[0] - 1] [coordinates[1]] === colorIndex;
-        // const isRightNeighbourIdentical = grid [coordinates[0]] [coordinates[1] + 1] === colorIndex;
-        // const isBottomNeighbourIdentical = grid [coordinates[0] + 1] [coordinates[1]] === colorIndex;
-        // const isLeftNeighbourIdentical = grid [coordinates[0]] [coordinates[1] - 1] === colorIndex;
+        if (didFindElement) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-        const isNotOnTopEdge = coordinates[0] > 0;
-        const isNotOnRightEdge = coordinates[1] < grid[0].length - 1;
-        const isNotOnBottomEdge = coordinates[0] < grid.length - 1;
-        const isNotOnLeftEdge = coordinates[1] > 0;
+    // console.log(isNeighbourAlreadyAdded([rowRootIndex, columnRootIndex]));
 
-        if (isNotOnTopEdge) {
-            // isTopNeighbourIdentical
-            if (grid[coordinates[0] - 1][coordinates[1]] === colorIndex) {
-                const newElement = [coordinates[0] - 1, coordinates[1]];
-                if (!identicalNeighbours.includes(newElement)) {
-                    identicalNeighbours.push(newElement);
+    while (isLookingIdenticalNeighbours) {
+
+        console.log(identicalNeighbours);
+
+        let didFindAnyIdenticalNeighbours = false;
+
+        identicalNeighbours.forEach(coordinates => {
+
+            const isNotOnTopEdge = coordinates[0] > 0;
+            const isNotOnRightEdge = coordinates[1] < grid[0].length - 1;
+            const isNotOnBottomEdge = coordinates[0] < grid.length - 1;
+            const isNotOnLeftEdge = coordinates[1] > 0;
+    
+            if (isNotOnTopEdge) {
+                // isTopNeighbourIdentical
+                if (grid[coordinates[0] - 1][coordinates[1]] === colorIndex) {
+                    const newElement = [coordinates[0] - 1, coordinates[1]];
+                    if (!isNeighbourAlreadyAdded(newElement)) {
+                        identicalNeighbours.push(newElement);
+                        didFindAnyIdenticalNeighbours = true;
+                    }
                 }
             }
-        }
-
-        if (isNotOnRightEdge) {
-            // isRightNeighbourIdentical
-            if (grid[coordinates[0]][coordinates[1] + 1] === colorIndex) {
-                const newElement = [coordinates[0], coordinates[1] + 1];
-                if (!identicalNeighbours.includes(newElement)) {
-                    identicalNeighbours.push(newElement);
+    
+            if (isNotOnRightEdge) {
+                // isRightNeighbourIdentical
+                if (grid[coordinates[0]][coordinates[1] + 1] === colorIndex) {
+                    const newElement = [coordinates[0], coordinates[1] + 1];
+                    if (!isNeighbourAlreadyAdded(newElement)) {
+                        identicalNeighbours.push(newElement);
+                        didFindAnyIdenticalNeighbours = true;
+                    }
                 }
             }
-        }
-
-        if (isNotOnBottomEdge) {
-            // isBottomNeighbourIdentical
-            if (grid[coordinates[0] + 1][coordinates[1]] === colorIndex) {
-                const newElement = [coordinates[0] + 1, coordinates[1]];
-                if (!identicalNeighbours.includes(newElement)) {
-                    identicalNeighbours.push(newElement);
+    
+            if (isNotOnBottomEdge) {
+                // isBottomNeighbourIdentical
+                if (grid[coordinates[0] + 1][coordinates[1]] === colorIndex) {
+                    const newElement = [coordinates[0] + 1, coordinates[1]];
+                    if (!isNeighbourAlreadyAdded(newElement)) {
+                        identicalNeighbours.push(newElement);
+                        didFindAnyIdenticalNeighbours = true;
+                    }
                 }
             }
-        }
-
-        if (isNotOnLeftEdge) {
-            // isLeftNeighbourIdentical
-            if (grid[coordinates[0]][coordinates[1] - 1] === colorIndex) {
-                const newElement = [coordinates[0], coordinates[1] - 1];
-                if (!identicalNeighbours.includes(newElement)) {
-                    identicalNeighbours.push(newElement);
+    
+            if (isNotOnLeftEdge) {
+                // isLeftNeighbourIdentical
+                if (grid[coordinates[0]][coordinates[1] - 1] === colorIndex) {
+                    const newElement = [coordinates[0], coordinates[1] - 1];
+                    if (!isNeighbourAlreadyAdded(newElement)) {
+                        identicalNeighbours.push(newElement);
+                        didFindAnyIdenticalNeighbours = true;
+                    }
                 }
             }
+        });
+
+        if (!didFindAnyIdenticalNeighbours) {
+            isLookingIdenticalNeighbours = false;
         }
-    });
+    }
 
-    console.log(identicalNeighbours);
-
-
-
-
-
-
-
-
-    // const isTopNeighbourIdentical = 
-    //     rowIndex === rowRootIndex - 1 && 
-    //     columnIndex === columnRootIndex && 
-    //     value === colorIndex;
-
-    // const isRightNeighbourIdentical = 
-    //     rowIndex === rowRootIndex && 
-    //     columnIndex === columnRootIndex + 1 && 
-    //     value === colorIndex;
-
-    // const isBottomNeighbourIdentical = 
-    //     rowIndex === rowRootIndex + 1 && 
-    //     columnIndex === columnRootIndex && 
-    //     value === colorIndex;
-
-    // const isLeftNeighbourIdentical = 
-    //     rowIndex === rowRootIndex && 
-    //     columnIndex === columnRootIndex - 1 && 
-    //     value === colorIndex;
-
-    // if (isTopNeighbourIdentical) {
-    //     score++;
-    //     return -1;
-    // }
-    // if (isRightNeighbourIdentical) {
-    //     score++;
-    //     return -1;
-    // }
-    // if (isBottomNeighbourIdentical) {
-    //     score++;
-    //     return -1;
-    // }
-    // if (isLeftNeighbourIdentical) {
-    //     score++;
-    //     return -1;
-    // }
+    //console.log(identicalNeighbours);
 
     return {
         modifiedGrid: grid,
