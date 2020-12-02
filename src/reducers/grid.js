f2b82818b789ebbccef9a6880a17777a804f0645
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-12-02 14:44:16 +0100

    Code cleaning

--- src/reducers/grid.js
+++ src/reducers/grid.js
@@ -143,23 +143,6 @@ const gridReducer = (state = [], action) => {
                         }
                     }
                 }
-                // newGrid.forEach((row, i) => {
-                //     row.forEach((value, j) => {
-                //         if (value === -1) {
-                //             isEmptyTileDetected = true;
-                //             // if it's top row, generate new color
-                //             if (i === 0) {
-                //                 newGrid[i][j] = colorIndexes[currentColorIndex];
-                //                 currentColorIndex ++;
-                //             } else {
-                //                 // replace with the value from the previous row
-                //                 const temp = newGrid[i][j];
-                //                 newGrid[i][j] = newGrid[i - 1][j];
-                //                 newGrid[i - 1][j] = temp;
-                //             }
-                //         }
-                //     });
-                // });
                 if (!isEmptyTileDetected) {
                     searchingForEmptyTiles = false;
                 }
